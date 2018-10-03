ebe1f4d34a73454712cb7fad4590ce331bc50992
Corné <cornebotma@hotmail.com>
2018-10-03 22:44:30 +0200

    finished mvp

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
-    document.getElementById("toggleFilter").visibility = "shown"
+    //Hide email adress on load of page
+    $('.toggleFilterEmail').toggle('hide');
 
-})
+    //Function to filter between email adress and mobile number
+    $('#myselectFilter').click(function(){
+        var value = $( "#myselectFilter option:selected" ).val();
+
+        if (value === '2') {
+            $('.toggleFilterNumber').hide() && $('.toggleFilterEmail').show();
+        } else {
+            $('.toggleFilterEmail').hide() && $('.toggleFilterNumber').show();
+        }  
+    }); 
+
+});
